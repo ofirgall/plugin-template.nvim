967c93f0f48a31da43296379ad3bd4bb22515e15
Ofir Gal <ofir@gal.software>
2023-02-16 17:08:51 +0200

    Added license chooser + updated README

--- README.md
+++ README.md
@@ -3,12 +3,11 @@
 ## Features
 * Basic `setup` function
 * Basic README
-* Generating vimdoc from [emmylua](https://github.com/numToStr/lemmy-help/blob/master/emmylua.md) using [lemmy-help](https://github.com/numToStr/lemmy-help) with github action. (./github/scripts/gen_doc.sh)
+* Generating vimdoc from [emmylua](https://github.com/numToStr/lemmy-help/blob/master/emmylua.md) using [lemmy-help](https://github.com/numToStr/lemmy-help) with github action. (./.github/scripts/gen_doc.sh)
+* Update usage from vimdoc (./.github/scripts/update_readme.sh)
 
 ## Usage
 ```bash
 # Run once
-setup.sh <PLUGIN_NAME> <PLUGIN_DESCRIPTION> <GITHUB_USERNAME>
+setup.sh <PLUGIN_NAME> <PLUGIN_DESCRIPTION> <GITHUB_USERNAME> <LICENSE>
 ```
-
-TODO: LICENSES
--- setup.sh
+++ setup.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env sh
 
-if [ "$#" -lt 3 ]; then
-	echo "setup.sh <PLUGIN_NAME> <PLUGIN_DESCRIPTION> <GITHUB_USERNAME>"
+if [ "$#" -lt 4 ]; then
+	echo "setup.sh <PLUGIN_NAME> <PLUGIN_DESCRIPTION> <GITHUB_USERNAME> <LICENSE>"
 fi
 
 PLUGIN_NAME=$1
 PLUGIN_DESCRIPTION=$2
 GITHUB_USER=$3
+LICENSE=$4
+
+if ! test -f $LICENSE; then
+	echo "Please choose license from LICENSES folder"
+	exit 1
+fi
+
+cp $LICENSE LICENSE
+
+# Replace [year] and [yyyy]
+YEAR=$(date +%Y)
+sed -i "s/\[year\]/$YEAR/g; s/\[yyyy\]/$YEAR/g" LICENSE
+# Replace [fullname] and [name of copyright owner]
+sed -i "s/\[fullname\]/$GITHUB_USER/g; s/\[name of copyright owner\]/$GITHUB_USER/g" LICENSE
 
 # Search & Replace {} placeholders
 find ! -path "./.git/*"  ! -name 'setup.sh' -type f -exec sed -i '' -e "s/{PLUGIN_NAME}/$PLUGIN_NAME/g; s/{PLUGIN_DESCRIPTION}/$PLUGIN_DESCRIPTION/g; s/{GITHUB_USERNAME}/$GITHUB_USER/g" {} \;
